8c7b2166fb53d82d3e3e88a04f8b2ab5c51b8d08
srikanthkamineni <srikanthkamineni@yahoo.com>
2014-06-22 04:32:14 -0400

    Updated run_analysis.R

--- Course-Project/run_analysis.R
+++ Course-Project/run_analysis.R
@@ -1,14 +1,14 @@
 #1. Merges the training and the test sets to create one data set.
 
 #1.a Read data in test folder
-test_Data <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/test/X_test.txt")
-test_Label <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/test/y_test.txt")
-test_Subject <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/test/subject_test.txt")
+test_Data <- read.table("./test/X_test.txt")
+test_Label <- read.table("./test/y_test.txt")
+test_Subject <- read.table("./test/subject_test.txt")
 
 #1.b Read data in train folder
-train_Data <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/train/X_train.txt")
-train_Label <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/train/y_train.txt")
-train_Subject <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/train/subject_train.txt")
+train_Data <- read.table("./train/X_train.txt")
+train_Label <- read.table("./train/y_train.txt")
+train_Subject <- read.table("./train/subject_train.txt")
 
 #1.c Merge into one data set
 merge_Data <- rbind(test_Data, train_Data)
@@ -16,7 +16,7 @@ merge_Label <- rbind(test_Label, train_Label)
 merge_Subject <- rbind(test_Subject, train_Subject)
 
 #2. Extracts only the measurements on the mean and standard deviation for each measurement.
-features <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/features.txt")
+features <- read.table("./features.txt")
 mean_std <- grep("mean\\(\\)|std\\(\\)", features[, 2])
 merge_Data <- merge_Data[, mean_std]
 names(merge_Data) <- gsub("\\(\\)", "", features[mean_std, 2])
@@ -25,7 +25,7 @@ names(merge_Data) <- gsub("std", "Std", names(merge_Data))
 names(merge_Data) <- gsub("-", "", names(merge_Data))
 
 #3. Uses descriptive activity names to name the activities in the data set
-activity <- read.table("c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/activity_labels.txt")
+activity <- read.table("./activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
 substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
@@ -36,7 +36,7 @@ names(merge_Label) <- "activity"
 #4. Appropriately labels the data set with descriptive variable names.
 names(merge_Subject) <- "subject"
 cleaned_Data <- cbind(merge_Subject, merge_Label, merge_Data)
-write.table(cleaned_Data, "c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/cleaned_data.txt")
+write.table(cleaned_Data, "cleaned_data.txt")
 
 #5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 subject_Len <- length(table(merge_Subject))
@@ -56,4 +56,4 @@ for(i in 1:subject_Len) {
         }
 }
 head(result)
-write.table(result, "c:/Srikanth/Coursera/Getting and Cleaning Data/Assignment/Dataset/average_data.txt")
+write.table(result, "average_data.txt")
